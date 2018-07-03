c6438d4849b276b0038121afc8dcfe551a757ad0
Stavros <Synodiporos@gmail.com>
2018-07-03 15:20:41 +0300

    Add files via upload

--- VM_Transmitter/System/SystemConstants.h
+++ VM_Transmitter/System/SystemConstants.h
@@ -9,13 +9,23 @@
 #define SYSTEM_SYSTEMCONSTANTS_H_
 
 
-#define HV_ANALOG_PIN A1
-#define BT_ANALOG_PIN A0
+#define HV_ANALOG_PIN (uint8_t) A1 //Á1
+#define BT_ANALOG_PIN (uint8_t) A0 //Á0
 
-#define HVPROBE_SPV (unsigned short int)64
+#define HVPROBE_SPV (unsigned short int)32
 #define HVPROBE_UNITS (char*)"kV"
 #define HVPROBE_MIN_RANGE (float)0
 #define HVPROBE_MAX_RANGE (float)3.77
+#define HVPROBE_MIN_MEAS_RANGE (float)0
+#define HVPROBE_MAX_MEAS_RANGE (float)45
 #define HVPROBE_BITRATE 128
+
+#define BATTM_DISC_VOLTAGE 3.0
+#define BATTM_FULL_VOLTAGE 4.2
+#define BATTM_MIN_RANGE (float)0
+#define BATTM_MAX_RANGE (float)3.77
+#define BATTM_BITRATE 1024
+#define BATTM_SPV (unsigned short int)32
+
 
 #endif /* SYSTEM_SYSTEMCONSTANTS_H_ */
